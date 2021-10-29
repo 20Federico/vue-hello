e7a5a5e93031ea2c6884ad7a7adf1c5c0e9ed9db
20Federico <fefe.molino@gmail.com>
2021-10-29 09:51:44 +0200

    error fix

--- js/main.js
+++ js/main.js
@@ -6,8 +6,10 @@ window.addEventListener('DOMContentLoaded', function () {
       el: '#vue',
       data: {
         h1: 'Titolo della pagina',
-        imgSrc: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fuploads-ssl.webflow.com%2F5e96913c9bac7c0b5cb3391c%2F5f44a7398c0cdf460857e744_img-image.jpg&f=1&nofb=1',
-        imgAlt: 'copertina'
+        image: {
+          imgSrc: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fuploads-ssl.webflow.com%2F5e96913c9bac7c0b5cb3391c%2F5f44a7398c0cdf460857e744_img-image.jpg&f=1&nofb=1',
+          imgAlt: 'copertina'
+        },
       },
       methods: {
         function () {
